5affdb8b54227049a0b2e7e7e842c0ada0081900
Lina Sachuk <sachuklina@gmail.com>
2020-10-14 23:26:30 -0600

    add weather-app folder

---
+++ weather-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const api = {
+  key: "1f3e1fdf655b1cda3b889eeaa488e092",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
